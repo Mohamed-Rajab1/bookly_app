f20b12e50fd7563f3ce473d7abd3b387ad541c38
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-21 18:28:43 +0300

    add font family to our app

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bokly_app/core/utils/styles.dart';
 import 'package:bokly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:bokly_app/features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:flutter/material.dart';
@@ -7,6 +8,14 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: const [CustomAppBar(), FeaturedBooksListView()]);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomAppBar(),
+        FeaturedBooksListView(),
+        SizedBox(height: 50),
+        Text('Best Seller', style: Styles.titleMedium),
+      ],
+    );
   }
 }
