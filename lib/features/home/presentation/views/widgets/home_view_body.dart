5a058396a4dc6220abaa7555f4e2faa317a5b55e
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-22 16:22:20 +0300

    best seller item part two

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -10,7 +10,7 @@ class HomeViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
@@ -18,6 +18,7 @@ class HomeViewBody extends StatelessWidget {
           FeaturedBooksListView(),
           SizedBox(height: 50),
           Text('Best Seller', style: Styles.textStyle18),
+           SizedBox(height: 20),
           BestSellerListViewItem(),
         ],
       ),
@@ -47,6 +48,19 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
           ),
+          const SizedBox(width: 30),
+          Column(
+            children:  [
+              SizedBox(
+                width : MediaQuery.of(context).size.width * .5,
+                child:const  Text('Hary Potter and the Goblet of Fire', 
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                 style: Styles.textStyle20),
+              ),
+              
+            ],
+          )
         ],
       ),
     );
