7d8fac7225da787c71d04b9af17965f78861266e
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-23 14:50:08 +0300

    create custom book details app bar

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -56,12 +56,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   void navigateToHome() {
-    Future.delayed(const Duration(seconds: 2), () {
-      // Get.to(
-      //   () => const HomeView(),
-      //   transition: Transition.fade,
-      //   duration: kTransitionDuration,
-      // );
+    Future.delayed(const Duration(seconds: 1), () {
       GoRouter.of(context).push(AppRouter.kHomeView);
     });
   }
