f927da3e35a5bfeb8f661ed51dc7ce1b56f5016b
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-25 18:09:32 +0300

    create api service

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bokly_app/core/errors/failures.dart';
+import 'package:bokly_app/features/home/data/models/book_models/book_model.dart';
+import 'package:bokly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRepo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
+    // TODO: implement fetchFeaturedBooks
+    throw UnimplementedError();
+  }
+}
