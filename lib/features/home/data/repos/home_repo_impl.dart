e519e7be53776c6b6d3a8fe7a6ad56426b98387b
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-26 15:31:51 +0300

    implement fetch newest books

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,29 @@
 import 'package:bokly_app/core/errors/failures.dart';
+import 'package:bokly_app/core/utils/api_service.dart';
 import 'package:bokly_app/features/home/data/models/book_models/book_model.dart';
 import 'package:bokly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl({required this.apiService});
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+        endpoint:
+            'volumes?filter=free-ebooks&sorting=newest&q=subject:programming&key=AIzaSyB4YHzDhf5bZWNsTo2b9otefhVdFXK-l10',
+      );
+      List<BookModel> books = [];
+
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
