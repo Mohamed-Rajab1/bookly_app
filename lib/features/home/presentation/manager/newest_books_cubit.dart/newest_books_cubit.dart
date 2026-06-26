24abcb67a6e27aab5b993185d66d0916f5074bb2
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-06-27 02:47:43 +0300

    newest books cubit done

--- lib/features/home/presentation/manager/newest_books_cubit.dart/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit.dart/newest_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bokly_app/features/home/data/models/book_models/book_model.dart';
+import 'package:bokly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
 }
