814b3a08e0e2526bfd0b71663882b0950d3d3a1e
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-07-07 14:28:51 +0300

    home repo

---
+++ lib/features/home/domain/repos/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:bokly_app/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRepo {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
