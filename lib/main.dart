06cf5cee9a8f14ae4e09d2e1d656cc5d320b5edf
Mohamed Rajab <mohamedragab132000@gmail.com>
2026-07-08 17:50:05 +0300

    register adabter and open featured box

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,20 @@ import 'package:bokly_app/constants.dart';
 import 'package:bokly_app/core/utils/app_router.dart';
 import 'package:bokly_app/core/utils/service_locator.dart';
 import 'package:bokly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bokly_app/features/home/domain/entities/book_entity.dart';
 import 'package:bokly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bokly_app/features/home/presentation/manager/newest_books_cubit.dart/newest_books_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
+void main() async {
   setupServiceLocator();
   runApp(const BooklyApp());
+
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class BooklyApp extends StatelessWidget {
